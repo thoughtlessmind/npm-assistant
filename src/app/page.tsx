607c3103206456a8d5bebb3cee4148a63ba8a3bb
Rajiv <51883613+thoughtlessmind@users.noreply.github.com>
2023-07-01 17:05:40 +0530

    fix: fixed theme and removed daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { ThemeProvider } from "next-themes";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -8,28 +7,26 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <ThemeProvider>
-      <main className="flex min-h-screen flex-col items-center p-24 bg-primary-10">
-        <h1>Hey Npm-assistant</h1>
-        <div className="flex items-center justify-center flex-grow w-full">
-          <input
-            placeholder="Search any npm package..."
-            className="border rounded px-4 py-2 w-1/2"
-            value={search}
-            onChange={(e) => {
-              setSearch(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                const params = new URLSearchParams({
-                  query: search,
-                }).toString();
-                router.push(`/search?${params}`);
-              }
-            }}
-          />
-        </div>
-      </main>
-    </ThemeProvider>
+    <main className="flex min-h-screen flex-col items-center p-24 bg-primary-10">
+      <h1>Hey Npm-assistant</h1>
+      <div className="flex items-center justify-center flex-grow w-full">
+        <input
+          placeholder="Search any npm package..."
+          className="border rounded px-4 py-2 w-1/2"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              const params = new URLSearchParams({
+                query: search,
+              }).toString();
+              router.push(`/search?${params}`);
+            }
+          }}
+        />
+      </div>
+    </main>
   );
 }
